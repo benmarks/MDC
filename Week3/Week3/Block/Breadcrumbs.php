4aab0e965064854c17e6e2a914908fe27e86e2d8
Benjamin Marks <ben@blueacorn.com>
2013-08-04 21:53:24 -0400

    Amended for style & to follow Magento constructor convention; ref Varien_Object::_construct()

--- Week3/Week3/Block/Breadcrumbs.php
+++ Week3/Week3/Block/Breadcrumbs.php
@@ -1,8 +1,9 @@
 <?php
-class BlueAcorn_Week3_Block_Breadcrumbs extends Mage_Page_Block_Html_Breadcrumbs {
-	function __construct()
+class BlueAcorn_Week3_Block_Breadcrumbs extends Mage_Page_Block_Html_Breadcrumbs
+{
+    public function _construct()
     {
-				$this->addCrumb('week3Crumb', array('label'=>'Google', 'title'=>'google', 'link'=>'http://google.com'));
-        parent::__construct();
+        $this->addCrumb('week3Crumb', array('label'=>'Google', 'title'=>'google', 'link'=>'http://google.com'));
+        parent::_construct();
     }
 }
